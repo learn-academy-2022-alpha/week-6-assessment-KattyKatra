3efbe296514b6d37164a422d705856fea6374d12
Kat <97001296+KattyKatra@users.noreply.github.com>
2022-03-29 17:15:20 -0700

    "First try"

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,24 +10,61 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+// a describe method that lists the name of the function OR naming of the particular test.
+// describe("hello", () => {
+//   it("returns a string that says hi", () => {
+//     expect(hello()).toEqual("hi")
+//   })
+// })
+
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
 // a) Create a test with an expect statement using the variable provided.
+
+// Create a function called theGuide that takes an object and returns each string in that object with capitals applied.
+// Input is an array of objects
+// Output is same array but capitalization is correctly applied to the names
+
+describe("theGuide", () => {
+  it("takes in an array of objects and returns an array with a sentence about each with their name capitalized", () => {
+    expect(theGuide(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
 
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 
 // b) Create the function that makes the test pass.
 
+const theGuide = () =>{
+  let name = people.name.split('').toUpperCase() + people.name.slice(1)
+  return name.map(theGuide).join('')
+}
+
+// I know it doesn't pass the test, can I please get a pointer in where to go? I didn't really get much from cohort-mates this time on this one
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+
+// Make a function called podgeFilter
+//Input is a mixed array
+//Filters out just the numbers
+// Divides numbers by 3
+//Outputs array of remainders
+
+describe("podgeFilter", () => {
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(podgeFilter(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(podgeFilter(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -37,10 +74,27 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 
 // b) Create the function that makes the test pass.
 
+const podgeFilter = (array) => {
+  let numbers = array.filter(Number.isFinite)
+  let divide = numbers.map(function(value) {return value % 3})
+  return divide
+}
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+
+// Create a function called cubeSum
+// Input is array of numbers
+// Grabs sum of numbers
+// Output is the cube of each sum
+
+describe("cubeSum", () => {
+  it("returns the sum of all the numbers cubed", () => {
+    expect(cubeSum(cubeAndSum1)).toEqual(99)
+    expect(cubeSum(cubeAndSum2)).toEqual(1125)
+  })
+})
 
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
@@ -49,3 +103,15 @@ const cubeAndSum2 = [0, 5, 10]
 
 
 // b) Create the function that makes the test pass.
+
+const cubeSum = (array) => {
+  let cube = array.map(x => Math.pow(x,3))
+  let sum = cube.reduce(function(sum, number) {
+    let result = sum + number
+    return result
+  }, 0)
+  return sum
+}
+
+// console.log(cubeSum(cubeAndSum1))
+// console.log(cubeSum(cubeAndSum2))
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1)Line 9 is the main controller for the app being called in and establishing the main class for ruby on rails. Line 10 is the start of method index which is used to convey all data in the database.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2)Line 13 is an instance variable and function combo to allow index to call upon all instances of BlogPost
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3)Lines 17-19 are setting up the show method for the app. Show is used to convey individual objects or instances of a class meeting the params stated (in this case just the id number)
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4)Lines 22-24 are setting up the new method allowing a form for the creation of new blog posts in this app. It would be tied to it's own html.erb page in this application to give the user a physical form to fill out.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5)Lines 28-33 are used to fully add a new post for the application. This works with the New method to add the post to index or, if it's invalid, prompt the user to retry.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6)Line 38 is the part of the edit method allowing the user to pull up a form to manipulate the data of that particular post in the database.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)Lines 44-49 are used in the update method to actually accept or deny the changes to the data in the database. The changes if valid will appear after the edit form is accepted and the post is refreshed, otherwise it will redirect the user back to the edit form.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8)Line 58 is used in the destroy method to redirect the user back to the post in case it actually was not deleted/destroyed.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) Lines 63 & 64 are the starts of the private method blog_post_params which is used to secure data in the rails app.
   private
   def blog_post_params
-    # ---10)
+    # ---10) Line 66 is the strict requirements and permissions for the params fields on the app. This one has a strict requirement of :blog_post and permits :title and :content to be passed through the params inputs. 
     params.require(:blog_post).permit(:title, :content)
   end
 end
